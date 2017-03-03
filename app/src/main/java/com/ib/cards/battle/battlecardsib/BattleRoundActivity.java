c388aaf14c75fb811505b8a108388b3066f7c567
André Iago Maracajá Albuquerque <andre.albuquerque@virtus.ufcg.edu.br>
2017-03-03 16:10:10 -0300

    save

--- app/src/main/java/com/ib/cards/battle/battlecardsib/BattleRoundActivity.java
+++ app/src/main/java/com/ib/cards/battle/battlecardsib/BattleRoundActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.LinearLayout;
 
 public class BattleRoundActivity extends AppCompatActivity {
 
@@ -13,8 +14,13 @@ public class BattleRoundActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_battle_round);
 
-       /* LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View opponentView = inflater.inflate(R.layout.card_item2, null);
+        View myView = inflater.inflate(R.layout.card_item2, null);
+        LinearLayout layoutOpponent = (LinearLayout) findViewById(R.id.ll_card_opponent);
+        LinearLayout layoutMyCard = (LinearLayout) findViewById(R.id.ll_my_card);
+        layoutOpponent.addView(opponentView, 0);
+        layoutMyCard.addView(myView, 0);
 
-        inflater.inflate(R.id.ll_card_opponent, R.layout.card_item2)*/
     }
 }
